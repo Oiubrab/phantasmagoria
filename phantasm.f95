1db6168040c2fbb7c862ee23986ec526c05ae675
Adrian <Lz4_8Fs3@protonmail.com>
2022-08-13 12:10:13 +1000

    the stream is poison

---
+++ phantasm.f95
@@ -0,0 +1,20 @@
+program phantasm
+
+implicit none
+
+character(len=100) :: reader
+integer,dimension(100) :: reader2
+integer :: lines
+
+!open(unit=1,access='stream', form='unformatted',file='big_balls1.bmp')
+open(unit=1,file='big_balls1.bmp')
+
+do lines=1,100
+	read(1,*) reader
+	print*,reader
+end do
+
+
+close(1)
+
+end program
