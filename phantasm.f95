8bc2f90475bdff31314b2d3fecfda1d1633bb937
Adrian <Lz4_8Fs3@protonmail.com>
2022-08-14 19:20:45 +1000

    extracting characters

--- phantasm.f95
+++ phantasm.f95
@@ -2,16 +2,22 @@ program phantasm
 
 implicit none
 
-character(len=:),allocatable :: reader
+!character(len=:),allocatable :: reader
+character(len=1000) :: reader
 integer,dimension(100) :: reader2
-integer :: lines,reader_num
+integer :: lines,reader_num,letter,letter_num
 
 !open(unit=1,access='stream', form='unformatted',file='big_balls1.bmp')
 open(unit=1,file='big_balls1.bmp')
 
-do lines=1,500
+do lines=1,2
 	read(1,*) reader
-	print*,reader
+	print*,trim(reader)
+	print*,len(reader)
+	do letter_num=1,2
+		print*,reader(letter_num:letter_num)
+!		read(reader(letter_num:letter_num),*) letter
+	end do
 end do
 
 
