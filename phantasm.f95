3cffaef188189ce2821d3036d3f7398aa8ee7924
Adrian <Lz4_8Fs3@protonmail.com>
2022-08-16 23:00:53 +1000

    erroro message output

--- phantasm.f95
+++ phantasm.f95
@@ -3,21 +3,25 @@ program phantasm
 implicit none
 
 !character(len=:),allocatable :: reader
-character(len=1000) :: reader
+character(len=500) :: reader,error
 integer,dimension(100) :: reader2
-integer :: lines,reader_num,letter,letter_num
+integer :: lines,reader_num,letter,letter_num,io
 
 !open(unit=1,access='stream', form='unformatted',file='big_balls1.bmp')
-open(unit=1,file='big_balls1.bmp')
+open(unit=1,file='big_balls1.bmp',iostat=io)
 
-do lines=1,2
-	read(1,*) reader
+do
+	read(1,*,iostat=io,iomsg=error) reader
+	if (io/=0) then
+		print*,error
+		exit
+	end if
 	print*,trim(reader)
-	print*,len(reader)
-	do letter_num=1,2
-		print*,reader(letter_num:letter_num)
-!		read(reader(letter_num:letter_num),*) letter
-	end do
+!	print*,len(reader)
+!	do letter_num=1,2
+!		print*,reader(letter_num:letter_num)
+!!		read(reader(letter_num:letter_num),*) letter
+!	end do
 end do
 
 
